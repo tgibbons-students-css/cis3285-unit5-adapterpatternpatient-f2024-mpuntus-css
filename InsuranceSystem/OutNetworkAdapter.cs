f67036e236595caf092671e6eef130cd593dec97
Mikita Puntus <150572042+mpuntus-css@users.noreply.github.com>
2024-10-08 04:39:12 -0500

    Part 1: Create a new OutNetworkAdapter class in the InsuranceSystem. And Part 2:  Add an OutNetwork patient to the Patient Repository
    Create a new OutNetworkAdapter class in the InsuranceSystem
    OutNetworkAdapter must implement the InsuranceInterface interface
    In OutNetworkAdapter, add a constructor that instantiates an OutNetworkPatient
    In OutNetworkAdapter,  getPatientName() should simply call getPatientName() in the OutNetworkPatient
    In OutNetworkAdapter,  getPolicyNumber() should call getPolicyNumber() in the OutNetworkPatient and convert the returned int to a String
    In OutNetworkAdapter,  CoverageAmount() should call CoveragePercent() in the OutNetworkPatient and and calculate the CoverageAmount by multiplying the CoveragePercent by the ProcedureCost
    In OutNetworkAdapter,  IsCovered() convert the PolicyNumber from a string to an int and then should call IsCovered() in the OutNetworkPatien
     Add an OutNetwork patient to the Patient Repository
    In the PatientRepository() constructor, instantiate a new patient using the OutPatientAdapter.

---
+++ InsuranceSystem/OutNetworkAdapter.cs
@@ -0,0 +1,43 @@
+﻿namespace Unit5_AdapterPatternPatient_Blazor.InsuranceSystem
+{
+    public class OutNetworkAdapter : InsuranceInterface
+    {
+        OutNetworkPatient patient;
+        public OutNetworkAdapter(string newName, int newPolicyNumber)
+        {
+            patient = new OutNetworkPatient(newName, newPolicyNumber);
+        }
+
+        public decimal CoverageAmount(int ProcedureID, decimal ProcedureCost)
+        {
+            return (patient.CoveragePercent(ProcedureCost) * ProcedureCost);
+        }
+
+        public string getPatientName()
+        {
+            return patient.getPatientName();
+        }
+
+        public string getPolicyNumber()
+        {
+            return patient.PolicyNumber.ToString();
+        }
+
+        public bool IsCovered(string patientName, string policyNumber)
+        {
+
+            //return true;
+            string covered = patient.IsCovered(patientName, policyNumber); 
+
+            if(covered.Equals("yes"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
